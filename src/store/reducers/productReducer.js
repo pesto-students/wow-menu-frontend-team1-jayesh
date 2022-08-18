e3a9cb77f5bbf69c653280b14148d7064b24751f
Pranshu-Sharma <sharmapranshu9@gmail.com>
2022-08-18 22:13:56 +0530

    customer ui changes merged

--- src/store/reducers/productReducer.js
+++ src/store/reducers/productReducer.js
@@ -1,69 +1,48 @@
-import axios from "axios";
-import camelize from "camelize";
-
-const PRODUCTS_REQUESTED = "PRODUCTS_REQUESTED";
-const PRODUCTS_REQUEST_SUCCESS = "PRODUCTS_REQUEST_SUCCESS";
-const PRODUCTS_REQUEST_FAILURE = "PRODUCTS_REQUEST_FAILURE";
 const SET_CATEGORY = "SET_CATEGORY";
+const SELECT_CATEGORY = "SELECT_CATEGORY";
+const SET_PAGE = "SET_PAGE";
 const SET_ITEM = "SET_ITEM";
 
-const fetchproductsRequest = () => ({
-  type: "PRODUCTS_REQUESTED",
-});
-const fetchproductsSuccess = (payload) => ({
-  type: "PRODUCTS_REQUEST_SUCCESS",
-  payload, // item data
-});
-const fetchproductsFailure = (payload) => ({
-  type: "PRODUCTS_REQUEST_FAILURE",
-  payload, // error message
-});
-
-export const getProducts = () => {
-  return function (dispatch) {
-    dispatch(fetchproductsRequest());
-    axios
-      .get(
-        "https://api.json-generator.com/templates/7fslmyyQ52AR/data?access_token=sr5evx3wg5ok41tjpvfyqb0d9aesmtr1usqiix4z",
-      )
-      .then((res) => {
-        dispatch(fetchproductsSuccess(camelize(res.data)));
-      })
-      .catch((error) => {
-        dispatch(fetchproductsFailure(error.message));
-      });
-  };
-};
 export const setCategory = (payload) => ({
-  type: "SET_CATEGORY", // category
+  type: "SET_CATEGORY", // init categories
+  payload,
+});
+export const selectCategory = (payload) => ({
+  type: "SELECT_CATEGORY", // categoryId
+  payload,
+});
+export const setCategoryPage = (payload) => ({
+  type: "SET_PAGE", // {category ,page}
   payload,
 });
 export const setItem = (payload) => ({
-  type: "SET_ITEM", // {product}
+  type: "SET_ITEM", // productId
   payload,
 });
 
 const initialState = {
-  items: [],
-  loading: null,
-  error: "",
+  categories: [],
   selectedItem: "",
   selectedCategory: "",
 };
 
 export default (state = initialState, action) => {
   switch (action.type) {
-    case PRODUCTS_REQUESTED: {
-      return { ...state, loading: true };
-    }
-    case PRODUCTS_REQUEST_SUCCESS: {
-      return { ...state, loading: false, items: action.payload };
-    }
-    case PRODUCTS_REQUEST_FAILURE: {
-      return { ...state, loading: false, items: [], error: action.payload };
-    }
     case SET_CATEGORY: {
+      return {
+        ...state,
+        categories: action.payload,
+        selectedCategory: action.payload[0],
+      };
+    }
+    case SELECT_CATEGORY: {
       return { ...state, selectedCategory: action.payload };
+    }
+    case SET_PAGE: {
+      const updatedCategory = state.categories;
+      const idx = updatedCategory.indexOf(action.payload.category);
+      updatedCategory[idx].page = action.payload.page;
+      return { ...state, selectedCategory: updatedCategory };
     }
     case SET_ITEM: {
       return { ...state, selectedItem: action.payload };
