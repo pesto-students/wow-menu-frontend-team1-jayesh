92ed65c0d255d26127b74001e506f5625d278062
shubham.agr <shubham.agr@media.net>
2022-09-08 00:45:04 +0530

    integrated logging using sentry

--- src/index.js
+++ src/index.js
@@ -1,9 +1,17 @@
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
+import * as Sentry from "@sentry/react";
+import { BrowserTracing } from "@sentry/tracing";
 import persistStore from "redux-persist/es/persistStore";
 import store from "./store/configureStore";
 import App from "./App";
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_DSN_URL,
+  integrations: [new BrowserTracing()],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 const persistor = persistStore(store);
